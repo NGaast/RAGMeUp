19b84e36885c788f90534781e938c9a391017482
NGaast <gaastraniels@gmail.com>
2024-12-03 16:35:25 +0100

    Fixed logger

--- server/server.py
+++ server/server.py
@@ -38,7 +38,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 load_bashrc()
 load_dotenv()
 
-test_graph = GraphRAG(logger)
+test_graph = GraphRAG()
 
 # Instantiate the RAG Helper class based on the environment configuration
 if any(os.getenv(key) == "True" for key in ["use_openai", "use_gemini", "use_azure", "use_ollama"]):
